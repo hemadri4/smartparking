3fb0bb45dfee4dcb1aa20aeeaafa337aeff0f008
hemadri4 <gluruhemadri4@gmail.com>
2026-09-20 20:00:46 +0530

    Simplify API client for CSRF handling

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,43 +5,4 @@ const api = axios.create({
     withCredentials: true,
 });
 
-function getCookie(name) {
-    const cookies = document.cookie.split(";");
-
-    for (let cookie of cookies) {
-        cookie = cookie.trim();
-
-        if (cookie.startsWith(name + "=")) {
-            return decodeURIComponent(
-                cookie.substring(name.length + 1)
-            );
-        }
-    }
-
-    return null;
-}
-
-api.interceptors.request.use(
-    (config) => {
-
-        if (
-            config.method &&
-            ["post", "put", "patch", "delete"].includes(
-                config.method.toLowerCase()
-            )
-        ) {
-
-            const csrfToken = getCookie("csrftoken");
-
-            if (csrfToken) {
-                config.headers["X-CSRFToken"] = csrfToken;
-            }
-        }
-
-        return config;
-    },
-
-    (error) => Promise.reject(error)
-);
-
 export default api;
