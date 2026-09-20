0210af7aa99a627011faf1d603a977479d615530
hemadri4 <gluruhemadri4@gmail.com>
2026-09-20 16:28:48 +0530

    Update navbar CSS and production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://smartparking-api-hemadri.onrender.com/api",
     withCredentials: true,
     xsrfCookieName: "csrftoken",
     xsrfHeaderName: "X-CSRFToken",
